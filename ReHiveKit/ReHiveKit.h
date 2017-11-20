2212a855f3f17971cb058184efc577f2bf6e065f
Sean Evans <sean.evans@flash.co.za>
2017-11-20 09:54:19 +0200

    Initial Commit

---
+++ ReHiveKit/ReHiveKit.h
@@ -0,0 +1,19 @@
+//
+//  ReHiveKit.h
+//  ReHiveKit
+//
+//  Created by Sean on 2017/11/20.
+//  Copyright © 2017 Flash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReHiveKit.
+FOUNDATION_EXPORT double ReHiveKitVersionNumber;
+
+//! Project version string for ReHiveKit.
+FOUNDATION_EXPORT const unsigned char ReHiveKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReHiveKit/PublicHeader.h>
+
+
